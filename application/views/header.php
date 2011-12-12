1a334adacda4ec2d14a1d56e8a175f9aa2fac9b4
Andy Hay <andy@andyhay.com>
2011-12-11 22:39:24 -0500

    changes

--- application/views/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<base href="http://localhost:8888/Dojo/" />
-
-<meta charset="utf-8">
-<title>Dojo Examples</title>
-
-<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojo/dojo.xd.js"></script>
